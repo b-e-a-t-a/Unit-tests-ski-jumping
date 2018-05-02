6f1863a02a4e8cad044d6845d4d1ade66d2ac17b
Beata Karolczak <karolczak.beata@gmail.com>
2018-05-02 12:10:19 +0200

    Add first test function

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -2,8 +2,8 @@ const calculateStylePoints = (styleNotes) => {
 
 	//const stylesNotes = [16,17,16.5,16,18];
 
-	let max = Math.max(...stylesNotes);
-  	let min = Math.min(...stylesNotes);
+	let max = Math.max(...styleNotes);
+  	let min = Math.min(...styleNotes);
 
   return styleNotes.reduce((acc,cur) => acc + cur, 0) - (max + min) 
 };
